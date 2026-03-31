060404c8970ad81ba548bf7438f9056fe6920cc6
Honour Shalom <shalomhonour1@gmail.com>
2026-03-31 16:28:29 +0100

    colour change

--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -4,7 +4,7 @@ function NextButton ({ goToNext, showFeedback, nextDisabled }) {
     onClick={goToNext} 
     disabled={nextDisabled}
     
-    className={`w-84.5 h-16.75 border border-[#DAD4D4] rounded-4xl cursor-pointer hover:scale-105 mt-31.75 ${showFeedback? "bg-[#4E1A1A]" : "bg-[#DAD4D4]"}`}
+    className={`w-84.5 h-16.75 border border-[#DAD4D4] rounded-4xl cursor-pointer hover:scale-105 mt-31.75 ${showFeedback? "bg-[#4E1A1A]" : "bg-[#DAD4D4]"} text-white`}
     >Next Question</button>
     )
 }
